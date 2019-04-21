8a90a26766cf06204a363c63e9ea8ba58635f7de
Yuta Katsuyama <ykatsuyama@id.iit.edu>
2019-04-21 23:49:31 +0000

    Automatically backed up by Learn

--- spec/birthday_spec.rb
+++ spec/birthday_spec.rb
@@ -13,7 +13,7 @@ describe "#happy_birthday" do
   end
 end
 
-## BONUS ##
+
 describe "#age_appropriate_birthday" do
   pending it "only prints the birthday greeting if the birthday kid 12 or younger" do
     birthday_kids = {
