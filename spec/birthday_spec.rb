1889426750224f272e189959f7784e9d7d703c35
Yuta Katsuyama <ykatsuyama@id.iit.edu>
2019-04-21 23:55:02 +0000

    Done.

--- spec/birthday_spec.rb
+++ spec/birthday_spec.rb
@@ -13,9 +13,9 @@ describe "#happy_birthday" do
   end
 end
 
-## BONUS ##
+
 describe "#age_appropriate_birthday" do
-  pending it "only prints the birthday greeting if the birthday kid 12 or younger" do
+   it "only prints the birthday greeting if the birthday kid 12 or younger" do
     birthday_kids = {
       "Timmy" => 9,
       "Sarah" => 6,
